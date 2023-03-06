9010d9c7c6f8cd13d7c2fbb620ce6fad1ff5873d
Arnav Patel <patelarnavm+github@gmail.com>
2023-03-05 21:08:50 -0500

    made everything dark mode, added to nutrition

--- src/pages/Meal.tsx
+++ src/pages/Meal.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Meal() {
-  return <div className="meal">Meal</div>;
+  return (
+    <div className="meal">
+      <div className="meal__top">
+        <h4 className="meal__heading">Lunch</h4>
+        <h4 className="meal__total">2400</h4>
+      </div>
+      <div className="meal__box">
+        <div className="meal__info">
+          <p className="meal__name">Giant Chocolate Chip Cookie wwwwwwwwwwww</p>
+          <p className="meal__amount">20 grams</p>
+        </div>
+        <p className="meal__calorie">120</p>
+      </div>
+      <div className="meal__box">
+        <div className="meal__info">
+          <p className="meal__name">Giant Chocolate Chip Cookie wwwwwwwwwwww</p>
+          <p className="meal__amount">20 grams</p>
+        </div>
+        <p className="meal__calorie">120</p>
+      </div>
+      <div className="meal__box">
+        <div className="meal__info">
+          <p className="meal__name">Giant Chocolate Chip Cookie wwwwwwwwwwww</p>
+          <p className="meal__amount">20 grams</p>
+        </div>
+        <p className="meal__calorie">120</p>
+      </div>
+      <button className="meal__button">Add Food</button>
+    </div>
+  );
 }
 
 export default Meal;
--- src/pages/Nutrition.tsx
+++ src/pages/Nutrition.tsx
@@ -16,25 +16,45 @@ function Nutrition() {
       <div className="nutrition__macros">
         <h3 className="nutrition__subheading">Macros</h3>
         <div className="nutrition__circles">
-          <div className="nutrition__protein">
+          <div className="nutrition__circle nutrition__calorie">
+            <h2 className="nutrition__grams">2000cal</h2>
+            <h5 className="nutrition__total">/ 3000cal</h5>
+            <p className="nutrition__label">Calories</p>
+          </div>
+          <div className="nutrition__circle nutrition__protein">
             <h2 className="nutrition__grams">130g</h2>
             <h5 className="nutrition__total">/ 160g</h5>
             <p className="nutrition__label">Protein</p>
           </div>
-          <div className="nutrition__fat">
+          <div className="nutrition__circle nutrition__fat">
             <h2 className="nutrition__grams">130g</h2>
             <h5 className="nutrition__total">/ 160g</h5>
             <p className="nutrition__label">Fat</p>
           </div>
-          <div className="nutrition__carb">
+          <div className="nutrition__circle nutrition__carb">
             <h2 className="nutrition__grams">130g</h2>
             <h5 className="nutrition__total">/ 160g</h5>
             <p className="nutrition__label">Carbs</p>
           </div>
+          <div className="nutrition__circle nutrition__water">
+            <h2 className="nutrition__grams">8oz</h2>
+            <h5 className="nutrition__total">/ 96g</h5>
+            <p className="nutrition__label">Water</p>
+          </div>
         </div>
       </div>
       <div className="nutrition__meals">
-        <div className="nutrition__add"></div>
+        <h3 className="nutrition__subheading">Meals</h3>
+        <div className="nutrition__add">
+          <input
+            type="text"
+            placeholder="Meal Title..."
+            className="nutrition__title"
+          />
+          <button className="nutrition__button">Add Meal</button>
+        </div>
+        <Meal />
+        <Meal />
         <Meal />
       </div>
     </div>
