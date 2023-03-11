9fdd9fb5ea53e769aba87db2f24d159be30c1636
Arnav Patel <patelarnavm+github@gmail.com>
2023-03-10 23:22:59 -0500

    worked on message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -5,7 +5,9 @@ function Message() {
     <div className="message">
       {true && (
         <div className="message__box">
-          <p className="message__text">Lorem ipsum dolor sit amet.</p>
+          <p className="message__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing.
+          </p>
           <p className="message__date">1:00 pm</p>
         </div>
       )}
