102ea24d329880f535ad05c626f67ae250ae3a6a
Arnav Patel <patelarnavm+github@gmail.com>
2023-03-16 21:58:20 -0400

    started coding for sets and exercise

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Set from "./Set";
 import { BsThreeDots } from "react-icons/bs";
 import "../pages/Routine.css";
 
 function Exercise() {
+  const [prevSets, setPrevSets] = useState([
+    { prevWeight: 200, prevReps: 10 },
+    { prevWeight: 300, prevReps: 8 },
+    { prevWeight: 400, prevReps: 6 },
+  ]);
+
+  const [sets, setSets] = useState([{ weight: 200, reps: 10 }]);
+
+  function handleClick() {
+    let setsArr = [...prevSets];
+    setsArr.push({ prevWeight: 0, prevReps: 0 });
+    setPrevSets(setsArr);
+  }
+
   return (
     <div className="exercise">
       <div className="exercise__top">
@@ -17,11 +31,20 @@ function Exercise() {
         <h4 className="exercise__subtitle">Notes</h4>
       </div>
       <div className="exercise__sets">
-        <Set />
-        <Set />
-        <Set />
+        {prevSets.map((prevSet, i) => (
+          <Set
+            key={i}
+            id={i + 1}
+            prevWeight={prevSet.prevWeight}
+            prevReps={prevSet.prevReps}
+            sets={sets}
+            setSets={setSets}
+          />
+        ))}
       </div>
-      <button className="exercise__add">+ Add Set</button>
+      <button className="exercise__add" onClick={handleClick}>
+        + Add Set
+      </button>
     </div>
   );
 }
--- src/components/Set.tsx
+++ src/components/Set.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../pages/Routine.css";
 
-function Set() {
+interface IProps {
+  id: number;
+  prevWeight: number;
+  prevReps: number;
+  sets: { weight: number; reps: number }[];
+  setSets: Function;
+}
+
+function Set(props: IProps) {
+  const [weight, setWeight] = useState<number>(0);
+  const [reps, setReps] = useState<number>(0);
+
+  function handleBlur() {}
+
   return (
     <div className="set">
       <div className="set__box">
-        <h5 className="set__set">1</h5>
-        <input type="number" placeholder="400" className="set__weight" />
-        <input type="number" placeholder="20" className="set__reps" />
+        <h5 className="set__set">{props.id}</h5>
+        <input
+          type="number"
+          placeholder={props.prevWeight.toString()}
+          value={weight || ""}
+          className="set__weight"
+          onChange={(event) => setWeight(Number(event.target.value))}
+          onBlur={handleBlur}
+        />
+        <input
+          type="number"
+          placeholder={props.prevReps.toString()}
+          value={reps || ""}
+          className="set__reps"
+          onChange={(event) => setReps(Number(event.target.value))}
+          onBlur={handleBlur}
+        />
       </div>
       <input type="text" placeholder="" className="set__notes" />
     </div>
